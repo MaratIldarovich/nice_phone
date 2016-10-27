c17996fbeacbe1f7efbe408a0ba56099b774f622
Vine <sqacool@yandex.ru>
2016-10-28 01:47:28 +0300

    Add some code

--- nice.js
+++ nice.js
@@ -6,75 +6,36 @@
  * @constructor
  */
 function NicePhoneInput(element, params){
-    var pos,
-        pressedKey,
-        oldStr;
-
-    var allowedKeys = ['Delete','Backspace','ArrowLeft','ArrowRight','F5'];
-    var blocks = params.pattern.split('{').map((block)=> block.split('}'));
-    console.log(blocks);
+    var oldPos;
 
     element.addEventListener('keydown', function(e){
-        pos = this.selectionStart;
-        oldStr = this.value;
-        console.log(window['testInput'] = this, 'down');
-        pressedKey = e.key;
-
-        if (pressedKey === 'ArrowLeft' || pressedKey === 'ArrowRight'){
-            //setMinPos();
-        }
-
-        if (!e.ctrlKey && allowedKeys.indexOf(pressedKey) === -1 && !(pressedKey * 1 >= 0) && !e.metaKey){
+        if (!e.ctrlKey && allowedKeys.indexOf(e.key) === -1 && !(e.key * 1 >= 0) && !e.metaKey){
             e.preventDefault();
+            return;
         }
 
-        //e.preventDefault();
+        oldPos = this.selectionStart;
     });
 
+    var allowedKeys = ['Delete','Backspace','ArrowLeft','ArrowRight','F5'];
+
     element.addEventListener('input', function(e){
-        console.log('input');
-        // if (this.value){
-        //     var newValue = '';
-        //     var newStr = this.value;
-        //     blocks.every((block, indx, arr)=>{
-        //         if (indx === 0){
-        //             newValue += block[0];
-        //         }
-        //         else{
-        //             //newValue += newStr.slice()block[0] + (block[1] || '');
-        //         }
-        //     });
-        //
-        //     var newPos;
-        //
-        //     var pastedText = null;
-        //
-        //     switch (pressedKey){
-        //         case 'Backspace':
-        //             newPos = pos - 1;
-        //             break;
-        //         case 'Delete':
-        //             newPos = pos;
-        //             break;
-        //         default:
-        //             newPos = pos + ((pastedText && pastedText.length) || 1);
-        //
-        //             for (let i = 0; i < specials.length; i++) {
-        //                 var str = specials[i];
-        //
-        //                 for (let j = pos; j < newPos; j++) {
-        //                     let specSymbolAhead = newStr.substr(j, str.length) === str;
-        //                     if (specSymbolAhead) {
-        //                         newPos = newPos + str.length;
-        //                     }
-        //                 }
-        //             }
-        //             break;
-        //     }
-        //
-        //     if (oldStr && (pos < oldStr.length)){
-        //         this.setSelectionRange(newPos, newPos);
-        //     }
-        // }
+        var str = this.value.replace(/[^0-9]/g, '');
+        var newStr = '';
+        var newPos = oldPos + 1;
+        for (var i = 0, j = 0; i < str.length && j < params.pattern.length; i++, j++) {
+            let sym = str[i];
+            let patternSym = params.pattern[j];
+            let patternSymIsNotANumber = !(patternSym * 1);
+            if (patternSymIsNotANumber){
+                newStr += patternSym;
+                j++;
+            }
+
+            newStr += sym;
+        }
+
+        this.value = newStr;
+        this.setSelectionRange(newPos, newPos);
     });
 }
