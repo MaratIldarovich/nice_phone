0cfcf0de735d4c84f8e78936d238b5312414321c
Vine <sqacool@yandex.ru>
2016-10-28 22:56:21 +0300

    Add new pattern mechanysm

--- nice.js
+++ nice.js
@@ -1,12 +1,16 @@
 /**
  * Creates new instance of nicePhone
- * @param element {HTMLElement}
  * @param params {Object}
+ * @param params.element {HTMLElement}
  * @param params.pattern {String} - pattern of inputed phone
  * @constructor
  */
-function NicePhoneInput(element, params){
-    var oldPos;
+function NicePhoneInput(params){
+    let element = params.element,
+        pattern = params.countryCode + params.pattern,
+        filter = new RegExp(`^${params.countryCode}/[^0-9]/g`),
+        allowedKeys = ['Delete','Backspace','ArrowLeft','ArrowRight','F5'],
+        oldPos;
 
     element.addEventListener('keydown', function(e){
         if (!e.ctrlKey && allowedKeys.indexOf(e.key) === -1 && !(e.key * 1 >= 0) && !e.metaKey){
@@ -17,19 +21,31 @@ function NicePhoneInput(element, params){
         oldPos = this.selectionStart;
     });
 
-    var allowedKeys = ['Delete','Backspace','ArrowLeft','ArrowRight','F5'];
+    element.addEventListener('paste', function(e){
+        oldPos = this.selectionStart;
+        var cData = e.clipboardData;
+        var pastedText = cData.getData('text');
+        console.log(pastedText);
+    });
+
+    function patternSymIsNotANumber(indx){
+        return pattern[indx] !== 'n';
+    }
 
     element.addEventListener('input', function(e){
-        var str = this.value.replace(/[^0-9]/g, '');
+        console.log('input', e);
+        var str = this.value.replace(filter, '');
+        console.log(str, filter);
         var newStr = '';
         var newPos = oldPos + 1;
-        for (var i = 0, j = 0; i < str.length && j < params.pattern.length; i++, j++) {
+
+        for (var i = 0, j = 0; i < str.length && j < pattern.length; i++, j++) {
             let sym = str[i];
-            let patternSym = params.pattern[j];
-            let patternSymIsNotANumber = !(patternSym * 1);
-            if (patternSymIsNotANumber){
-                newStr += patternSym;
+
+            while (patternSymIsNotANumber(j)){
+                newStr += pattern[j];
                 j++;
+                if (newPos === j) newPos++;
             }
 
             newStr += sym;
