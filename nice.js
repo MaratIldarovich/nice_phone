e48f0de44dd4f40a618cfbfaa698745def99b5f2
Vine <sqacool@yandex.ru>
2016-10-29 22:25:51 +0300

    Add firstSyms handling

--- nice.js
+++ nice.js
@@ -7,10 +7,19 @@
  */
 function NicePhoneInput(params){
     let element = params.element,
-        pattern = params.countryCode + params.pattern,
-        filter = new RegExp(`^${params.countryCode}/[^0-9]/g`),
+        pattern = params.pattern,
+        firstSyms = '',
         allowedKeys = ['Delete','Backspace','ArrowLeft','ArrowRight','F5'],
         oldPos;
+
+    for (var i = 0; i < pattern.length; i++) {
+        let sym = pattern[i];
+        if (sym === 'n') break;
+        firstSyms += sym;
+    }
+
+    pattern = deleteFirstSyms(pattern);
+    console.log(pattern);
 
     element.addEventListener('keydown', function(e){
         if (!e.ctrlKey && allowedKeys.indexOf(e.key) === -1 && !(e.key * 1 >= 0) && !e.metaKey){
@@ -25,24 +34,25 @@ function NicePhoneInput(params){
         oldPos = this.selectionStart;
         var cData = e.clipboardData;
         var pastedText = cData.getData('text');
-        console.log(pastedText);
     });
 
-    function patternSymIsNotANumber(indx){
-        return pattern[indx] !== 'n';
+    function deleteFirstSyms(str){
+        if (str.lastIndexOf(firstSyms) === 0){
+            str = str.slice(firstSyms.length);
+        }
+        return str;
     }
 
-    element.addEventListener('input', function(e){
-        console.log('input', e);
-        var str = this.value.replace(filter, '');
-        console.log(str, filter);
+    function processStr(str){
+        str = deleteFirstSyms(str);
+        str = str.replace(/[^0-9]/g,'');
         var newStr = '';
         var newPos = oldPos + 1;
 
         for (var i = 0, j = 0; i < str.length && j < pattern.length; i++, j++) {
             let sym = str[i];
 
-            while (patternSymIsNotANumber(j)){
+            while (pattern[j] !== 'n'){
                 newStr += pattern[j];
                 j++;
                 if (newPos === j) newPos++;
@@ -51,7 +61,15 @@ function NicePhoneInput(params){
             newStr += sym;
         }
 
-        this.value = newStr;
-        this.setSelectionRange(newPos, newPos);
+        newStr = firstSyms + newStr;
+
+        return {newPos, newStr};
+    }
+
+    element.addEventListener('input', function(e){
+        var data = processStr(this.value);
+
+        this.value = data.newStr;
+        this.setSelectionRange(data.newPos, data.newPos);
     });
 }
